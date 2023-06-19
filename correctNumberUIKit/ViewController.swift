ff2b1e6e6d6868ebed271fb4f1489a8f774d201d
jujinhyeong <mhn03148@naver.com>
2023-06-19 16:55:59 +0900

    1.사용자가 화면의 0부터 10 사이의 숫자를 선택한다 2.사용자에게 정답과 동일한지를 알려준다 3.정답이 아닌 경우 정답과 비교해 크다/작다 여부를 알려준다 4.사용자는 "새로운 맞추기 게임 시작" 버튼을 눌러 새 게임을 시작한다

--- correctNumberUIKit/ViewController.swift
+++ correctNumberUIKit/ViewController.swift
@@ -9,11 +9,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomNumber: Int = Int.random(in: 0...9)
+    var userNumber: Int = 0
+    @IBOutlet weak var numberLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func zeroButton(_ sender: Any) {
+        userNumber = 0
+        isCorrectNumber(userNumber)
+        print("\(randomNumber), \(userNumber)")
+    }
+    
+    @IBAction func oneButton(_ sender: Any) {
+        userNumber = 1
+        isCorrectNumber(userNumber)
+        print("\(randomNumber), \(userNumber)")
+    }
+    
+    @IBAction func twoButton(_ sender: Any) {
+        userNumber = 2
+        isCorrectNumber(userNumber)
+        print("\(randomNumber), \(userNumber)")
+    }
+    @IBAction func threeButton(_ sender: Any) {
+        userNumber = 3
+        isCorrectNumber(userNumber)
+        print("\(randomNumber), \(userNumber)")
+    }
+    @IBAction func fourButton(_ sender: Any) {
+        userNumber = 4
+        isCorrectNumber(userNumber)
+        print("\(randomNumber), \(userNumber)")
+    }
+    @IBAction func fiveButton(_ sender: Any) {
+        userNumber = 5
+        isCorrectNumber(userNumber)
+        print("\(randomNumber), \(userNumber)")
+    }
+    @IBAction func sixButton(_ sender: Any) {
+        userNumber = 6
+        isCorrectNumber(userNumber)
+        print("\(randomNumber), \(userNumber)")
+    }
+    @IBAction func sevenButton(_ sender: Any) {
+        userNumber = 7
+        isCorrectNumber(userNumber)
+        print("\(randomNumber), \(userNumber)")
+    }
+    @IBAction func eightButton(_ sender: Any) {
+        userNumber = 8
+        isCorrectNumber(userNumber)
+        print("\(randomNumber), \(userNumber)")
+    }
+    @IBAction func nineButton(_ sender: Any) {
+        userNumber = 9
+        isCorrectNumber(userNumber)
+        print("\(randomNumber), \(userNumber)")
+    }
+    
+    @IBAction func newGameButton(_ sender: Any) {
+        randomNumber = Int.random(in: 0...9)
+    }
+    func isCorrectNumber(_ index: Int){
+        if randomNumber == index {
+            numberLabel.text = "숫자를 맞췄습니다."
+        }
+        else {
+            if index > randomNumber {
+                numberLabel.text = "숫자가 큽니다 낮춰보세요"
+            }
+            else if index < randomNumber {
+                numberLabel.text = "숫자가 작습니다 높여보세요"
+            }
+        }
+    }
 }
 
